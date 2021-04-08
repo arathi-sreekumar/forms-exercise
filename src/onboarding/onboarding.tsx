8b8182ebd2db36731787e046b3bec62064eb45b9
Arathi Sreekumar <arathi.m.s@gmail.com>
2021-04-08 23:13:13 +0100

    feat: initial styling and setting up an onboarding component

---
+++ src/onboarding/onboarding.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './onboarding.scss';
+
+export const OnBoarding = () => {
+  return (
+    <main className="main">
+      <h1>Onboarding</h1>
+    </main>
+  );
+}
